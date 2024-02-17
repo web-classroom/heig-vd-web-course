a94a5dcc764642aacce81d54054f6de97da415bd
Vincent Guidoux <vincent.guidoux@gmail.com>
2024-02-17 13:00:40 +0100

    Update .gitignore (#7)

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+## Update packages list
+sudo apt update
+
+## Install packages to optimize images (jpegoptim, optipng)
+sudo apt install --yes jpegoptim optipng
+
+## Install packages to optimize documents (ps2pdf)
+sudo apt install --yes ghostscript
+
+## Install Typst
+wget https://github.com/typst/typst/releases/download/v0.10.0/typst-x86_64-unknown-linux-musl.tar.xz -O /tmp/typst.tar.xz
+
+sudo tar --extract --file /tmp/typst.tar.xz --strip-components=1 --directory /usr/local/bin
+
+rm /tmp/typst.tar.xz
+
+# MkDocs dependencies
+pip3 install \
+	cairosvg \
+	mkdocs-git-revision-date-localized-plugin \
+	mkdocs-glightbox \
+	mkdocs-material \
+	mkdocs-minify-plugin \
+    mkdocs-awesome-pages-plugin \
+	mkdocs_puml \
+	pillow

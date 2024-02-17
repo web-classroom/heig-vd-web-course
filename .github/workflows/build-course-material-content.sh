acf2ccb3fcf77060d151da437b4ca20042a83e67
Vincent Guidoux <vincent.guidoux@gmail.com>
2024-02-17 18:22:31 +0100

    Refactor build scripts for course material and presentation content

--- .github/workflows/build-course-material-content.sh
+++ .github/workflows/build-course-material-content.sh
@@ -9,19 +9,19 @@ apk add --no-cache \
     weasyprint
 
 # Convert Markdown files to HTML
-find . -type f -name "COURSE_MATERIAL.md" -mindepth 3 -maxdepth 2 -exec sh -c ' \
+find . -type f -name "COURSE_MATERIAL.md" -exec sh -c ' \
     sed "/<details>/,/<\/details>/d" "$1" | pandoc --from=gfm --to=html --standalone --output "${1%.md}.html" \
 ' sh {} \;
 
 # Convert HTML files to PDF
-find . -type f -name "COURSE_MATERIAL.html" -mindepth 3 -maxdepth 2 -exec sh -c ' \
+find . -type f -name "COURSE_MATERIAL.html" -exec sh -c ' \
     weasyprint --stylesheet .github/workflows/course-material-template/style.css "$1" "${1%.html}.pdf" \
 ' sh {} \;
 
 # Rename all PDF files to the name of their parent directory
-find . -type f -name "COURSE_MATERIAL.pdf" -mindepth 3 -maxdepth 2 -exec sh -c ' \
+find . -type f -name "COURSE_MATERIAL.pdf" -exec sh -c ' \
     mv "$1" "$(dirname "$1")/$(basename "$(dirname "$1")")-course-material.pdf" \
 ' sh {} \;
 
 # Delete all HTML files
-find .  -type f -name "COURSE_MATERIAL.html" -mindepth 3 -maxdepth 2 -delete
+find .  -type f -name "COURSE_MATERIAL.html" -delete
